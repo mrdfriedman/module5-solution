b72edc3fb24169de14ebbb46d5a2699756564099
David <closeupman@gmail.com>
2017-02-19 17:31:14 -0800

    some changes

--- js/script.js
+++ js/script.js
@@ -83,10 +83,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML,
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
+
 
 
 // Builds HTML for the home page based on categories array
@@ -103,7 +104,6 @@ function buildAndShowHomeHTML (categories) {
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
-
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -115,17 +115,18 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
+
       var homeHtmlToInsertIntoMainPage = homeHtml;
       var categoryShortName = "'" + chosenCategoryShortName + "'";
-	  homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage, "randomCategoryShortName", categoryShortName);
+	    homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage, "randomCategoryShortName", categoryShortName);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-    
-      homeHtmlToInsertIntoMainPage = "<section class='row'>"+homeHtmlToInsertIntoMainPage+"</section>";
-	  insertHtml("#main-content",homeHtmlToInsertIntoMainPage);
+      homeHtmlToInsertIntoMainPage = "<section class='row'>" + homeHtmlToInsertIntoMainPage + "</section>";
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
